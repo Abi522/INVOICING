ce8c268e21c1b4ab2d3378235577e9f46c106360
Abi522 <118547562+Abi522@users.noreply.github.com>
2024-01-09 22:44:09 +0530

    Add files via upload

---
+++ invoicing_project/invoicing_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import InvoiceListCreateView, InvoiceRetrieveUpdateDestroyView, \
+    InvoiceDetailListCreateView, InvoiceDetailRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('invoices/', InvoiceListCreateView.as_view(), name='invoice-list-create'),
+    path('invoices/<int:pk>/', InvoiceRetrieveUpdateDestroyView.as_view(), name='invoice-retrieve-update-destroy'),
+    path('invoice-details/', InvoiceDetailListCreateView.as_view(), name='invoice-detail-list-create'),
+    path('invoice-details/<int:pk>/', InvoiceDetailRetrieveUpdateDestroyView.as_view(),
+         name='invoice-detail-retrieve-update-destroy'),
+]
